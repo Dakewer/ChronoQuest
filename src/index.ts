2b1b8706b4030ad0a256c86c54d9318271afff6c
Diego <diego.orozcom@iteso.mx>
2026-03-10 14:01:17 -0600

    Actualizacion 3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 "use strict";
 
-// Nota para diego, esto es la app
-console.log('Happy developing ✨')
+import express from "express";
+import path from "path";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.ts"));
+});
+
+app.listen(port, () => {
+    console.log("http://localhost:" + port);
+})
